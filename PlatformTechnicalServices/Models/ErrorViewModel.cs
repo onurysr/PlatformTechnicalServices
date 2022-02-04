95ebe3d7aa809b9af8589b61e6ad35a7cdc8fd1a
onurysr <onurysr97@gmail.com>
2022-02-04 17:15:06 +0300

    Add project files.

---
+++ PlatformTechnicalServices/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PlatformTechnicalServices.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
