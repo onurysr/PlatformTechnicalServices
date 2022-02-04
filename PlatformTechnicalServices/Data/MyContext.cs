204e2d910344893128945581abf95e5894f0b380
onurysr <onurysr97@gmail.com>
2022-02-04 19:57:09 +0300

    Application role ve user eklendi, MyContext oluştu

---
+++ PlatformTechnicalServices/Data/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PlatformTechnicalServices.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformTechnicalServices.Data
+{
+    public class MyContext : IdentityDbContext<ApplicationUser, ApplicationRole,string>
+    {
+        public MyContext(DbContextOptions options):base(options)
+        {
+
+        }
+    }
+    
+}
