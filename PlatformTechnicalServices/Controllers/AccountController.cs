db3e0a17d679250955fd897ea8cc2a1bc8b1ab45
onurysr <onurysr97@gmail.com>
2022-02-07 16:30:56 +0300

    UserManager Injcet edildi

--- PlatformTechnicalServices/Controllers/AccountController.cs
+++ PlatformTechnicalServices/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PlatformTechnicalServices.Models.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,19 @@ namespace PlatformTechnicalServices.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
+
+        
     }
 }
