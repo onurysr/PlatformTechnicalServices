818d1d7e45d9b90f5a537399c17b90873f749d66
onurysr <onurysr97@gmail.com>
2022-02-07 15:51:35 +0300

    RoleModels Eklendi

---
+++ PlatformTechnicalServices/Models/RoleModels.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformTechnicalServices.Models
+{
+    public static class RoleModels
+    {
+        public static string Admin = "Admin";
+        public static string Operator = "Operator";
+        public static string Teknisyen = "Teknisyen";
+        public static string Musteri = "Musteri";
+        public static string Passive = "Passive";
+
+        public static ICollection<string> Roles => new List<string> { Admin, Operator, Teknisyen, Musteri, Passive };
+
+    }
+}
