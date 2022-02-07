3128b96c572406fc68a59e6429a6a432593b936a
resulyag <resulyag@gmail.com>
2022-02-07 19:05:52 +0300

    Register ve login actionları tamamlandı

--- PlatformTechnicalServices/Controllers/AccountController.cs
+++ PlatformTechnicalServices/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PlatformTechnicalServices.Models.Identity;
+using PlatformTechnicalServices.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,23 +13,91 @@ namespace PlatformTechnicalServices.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.UserName), "Bu kullanıcı adı daha önce kayıt edilmiştir");
+                return View(model);
+            }
+
+            user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.Email), "Bu email adresi daha önce kayıt edilmiştir");
+                return View(model);
+            }
+
+            user = new ApplicationUser()
+            {
+                Email = model.Email,
+                Name = model.Name,
+                UserName = model.UserName,
+                Surname = model.Surname
+            };
+
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                //kullanıcıya rol atama
+                //email onay maili
+                //login sayfasına yönlendirme
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Bir hata oluştu");
+                return View(model);
+            }
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
         }
 
-        
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe,true);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError(String.Empty, "Kullanıcı veya şifre hatalı");
+                return View(model);
+            }
+        }
     }
 }
