c372f577fb4098904d85fcd418f0a9a650f04788
onurysr <onurysr97@gmail.com>
2022-02-08 16:22:38 +0300

    Mail Sender Service ayarlandı

---
+++ PlatformTechnicalServices/Services/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using PlatformTechnicalServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformTechnicalServices.Services
+{
+    public interface IEmailSender
+    {
+        Task SendAsync(EmailMessage message);
+    }
+}
