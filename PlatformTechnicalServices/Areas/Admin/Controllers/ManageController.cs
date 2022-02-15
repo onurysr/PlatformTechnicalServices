1dbdb1e32af7049b9d54b942158d23fad7fd1a9a
resulyag <resulyag@gmail.com>
2022-02-15 18:07:46 +0300

    admin users ve detay sayfası oluştu

--- PlatformTechnicalServices/Areas/Admin/Controllers/ManageController.cs
+++ PlatformTechnicalServices/Areas/Admin/Controllers/ManageController.cs
@@ -1,14 +1,51 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using PlatformTechnicalServices.Models.Identity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace PlatformTechnicalServices.Areas.Admin.Controllers
 {
     [Authorize(Roles = "Admin")]
     public class ManageController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+
+        public ManageController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult Users()
+        {
+            var a = _userManager.Users;
+            return View(a);
+        }
+        public async Task<IActionResult> Details(string? id)
+        {
+            var a = await _userManager.FindByIdAsync(id);
+            ViewBag.Roles = GetRoleList();
+            return View(a);
+        }
+
+        private List<SelectListItem> GetRoleList()
+        {
+            var roles = _roleManager.Roles;
+            var rolList = new List<SelectListItem>();
+
+            foreach (var role in roles)
+            {
+                rolList.Add(new SelectListItem(role.Name, role.Id.ToString()));
+            }
+            return rolList;
         }
     }
 }
---
+++ PlatformTechnicalServices/Areas/Admin/Controllers/UserApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using PlatformTechnicalServices.Models.Identity;
+
+namespace PlatformTechnicalServices.Areas.Admin.Controllers
+{
+    [Route("api/[Controller]/[Action]")]
+    [Authorize(Roles ="Admin")]
+    public class UserApiController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserApiController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+    }
+}
