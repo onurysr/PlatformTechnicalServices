3b4d9e1506af0a6b40d9859bdf334ce76d2aedea
gkrctn <gkr.cetin@gmail.com>
2022-02-16 20:24:45 +0300

    manage delete yapıldı

--- PlatformTechnicalServices/Areas/Admin/Controllers/ManageController.cs
+++ PlatformTechnicalServices/Areas/Admin/Controllers/ManageController.cs
@@ -135,5 +135,25 @@ namespace PlatformTechnicalServices.Areas.Admin.Controllers
 
             return LocalRedirect($"~/admin/manage/update/{user.Id}");
         }
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null) return NotFound();
+
+            var result = await _userManager.DeleteAsync(user);
+
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty, ModelState.ToFullErrorString());
+            }
+
+            TempData["mesaj"] = "Silme işlemi başarılı";
+
+            return LocalRedirect($"~/admin/manage/users");
+
+
+        }
+
+
     }
 }
