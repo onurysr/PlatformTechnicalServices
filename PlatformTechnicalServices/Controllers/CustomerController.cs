14292c5c5c8d1d50a9c691702c1cd3848f9cc373
onurysr <onurysr97@gmail.com>
2022-02-17 20:25:03 +0300

    Customer Controlelr yarım kaldı

---
+++ PlatformTechnicalServices/Controllers/CustomerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformTechnicalServices.Controllers
+{
+    [Authorize]
+    public class CustomerController : Controller
+    {
+        [HttpGet]
+        public IActionResult AddFault()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddFault()
+        {
+            return View();
+        }
+    }
+}
---
+++ PlatformTechnicalServices/ViewModels/AddFaultViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlatformTechnicalServices.ViewModels
+{
+    public class AddFaultViewModel
+    {
+        public string UserId { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
