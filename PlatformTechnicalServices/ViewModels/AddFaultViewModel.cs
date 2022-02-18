6faf20ce03f8893bc93aa55305fa4555311d4d26
Onur Yaşar <onurysr97@gmail.com>
2022-02-18 20:16:39 +0300

    data anno. eklendi

--- PlatformTechnicalServices/ViewModels/AddFaultViewModel.cs
+++ PlatformTechnicalServices/ViewModels/AddFaultViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PlatformTechnicalServices.ViewModels
 {
+    
     public class AddFaultViewModel
     {
+        //[Required(ErrorMessage ="Telefon Numarası Girilmek Zorunda")]
+        [StringLength(11,MinimumLength =10,ErrorMessage ="Telefon numarası en az 10 haneli olmalıdır.")]
         public string PhoneNumber { get; set; }
+        //[Required(ErrorMessage = "Adres Alanı Girilmek Zorunda")]
         public string Address { get; set; }
+        //[Required(ErrorMessage = "Açıklama Alanı Girilmek Zorunda")]
         public string Description { get; set; }
     }
 }
