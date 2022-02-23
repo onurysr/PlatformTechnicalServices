d3eda8d646c4cbab3e624717c3002d8dd20b76e2
resulyag <resulyag@gmail.com>
2022-02-23 20:23:26 +0300

    Fault api GetUserLookup

--- PlatformTechnicalServices/Areas/Admin/Controllers/FaultApiController.cs
+++ PlatformTechnicalServices/Areas/Admin/Controllers/FaultApiController.cs
@@ -1,8 +1,10 @@
 ﻿using DevExtreme.AspNet.Data;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PlatformTechnicalServices.Data;
 using PlatformTechnicalServices.Extensions;
+using PlatformTechnicalServices.Models.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +17,13 @@ namespace PlatformTechnicalServices.Areas.Admin.Controllers
     public class FaultApiController : Controller
     {
         private readonly MyContext _DbContext;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public FaultApiController(MyContext DbContext)
+
+        public FaultApiController(MyContext DbContext, UserManager<ApplicationUser> userManager)
         {
             _DbContext = DbContext;
+            this._userManager = userManager;
         }
 
         [HttpGet]
@@ -27,6 +32,19 @@ namespace PlatformTechnicalServices.Areas.Admin.Controllers
             var data = _DbContext.FaultRecords;
 
             return Ok(DataSourceLoader.Load(data, loadoptions));
+        }
+
+        //user bilgilerini getiren bir lookup kodu yazılacak
+        public async Task<IActionResult> GetUserLookup(string id)
+        {
+            var data = await _userManager.FindByIdAsync(id);
+            var model = new
+            {
+                Key = data.Id,
+                Value = $"{data.Name} {data.Surname}"
+            };
+
+            return Ok(model);
         }
     }
 }
